2b7e01564b8e932ca218ca58592e85c9ff3336ad
rupesh <rursharma02@gmail.com>
2024-11-10 20:31:05 +0530

    change logo

---
+++ src/screens/CommentScreen.jsx
@@ -0,0 +1,17 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+import CustomBottomSheet from '../components/bottomSheet/BottomSheet';
+
+const CommentScreen = () => {
+  return (
+    <CustomBottomSheet snapPoint={[""]}>
+
+    </CustomBottomSheet>
+);
+};
+
+export default CommentScreen;
+
+const styles = StyleSheet.create({
+
+});
